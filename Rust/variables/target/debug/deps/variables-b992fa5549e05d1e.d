38942fa18fe366e8bea6ee0b69a00ee7f6dd9c85
Alenii <crypticmaven@gmail.com>
2023-05-31 12:21:23 +0100

    variables

---
+++ Rust/variables/target/debug/deps/variables-b992fa5549e05d1e.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/danie/Code/Projects/practice-makes-perf/Rust/variables/target/debug/deps/variables-b992fa5549e05d1e: src/main.rs
+
+/mnt/c/Users/danie/Code/Projects/practice-makes-perf/Rust/variables/target/debug/deps/variables-b992fa5549e05d1e.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/danie/Code/Projects/practice-makes-perf/Rust/variables/target/debug/variables: /mnt/c/Users/danie/Code/Projects/practice-makes-perf/Rust/variables/src/main.rs
