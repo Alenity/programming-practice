8e0a89ae6f0b084156015e32b5788d998d984cad
Alenii <crypticmaven@gmail.com>
2023-06-06 14:21:45 +0100

    refacotring code to make it cleaner

--- Rust/project-2 (Minigrep)/src/main.rs
+++ Rust/project-2 (Minigrep)/src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, process};
+use project_2::Config;
+
 fn main() {
-    println!("stuff");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = project_2::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+
 }
+
+
