857f3ca4e347a6d7232cc034868e53cb5e19dc3e
Alenii <crypticmaven@gmail.com>
2024-01-22 19:54:12 +0000

    Beginning

--- C++/ScientificCalc/main.cpp
+++ C++/ScientificCalc/main.cpp
@@ -4,19 +4,25 @@
 
 using namespace std;
 
-class Calc {
+class Functions {
 public:
 	int sum(int a, int b) {
 		return a + b;
 	}
 	int difference(int a, int b) {
-		return a - b;
+		return sum(a, -b);
 	}
 	int product(int a, int b) {
 		return a * b;
 	}
 	int quotient(int a, int b) {
-		return a / b;
+		return product(a, 1/b);
+	}
+	int exponent(int a, int b) {
+		int res = 1;
+		for(int i=0;i<b;i++){
+			res *= a;
+		}
 	}
 
 };
@@ -32,7 +38,7 @@ public:
 
 };
 
-int main() {
+int main(int argc, char *argv[]) {
 
-}
+} 
 
