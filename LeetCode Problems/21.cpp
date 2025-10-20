d5ba8ce390f20f305bd142e89a6789de2c3df728
A7 <danieladeniyi17@outlook.com>
2025-10-20 22:02:53 +0100

    Update 21.cpp

--- LeetCode Problems/21.cpp
+++ LeetCode Problems/21.cpp
@@ -8,43 +8,44 @@ struct ListNode {
 	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+
 class Solution {
 public:
+	ListNode* min(ListNode* one, ListNode* two) {
+		if (one != nullptr && two != nullptr) {
+			if (one->val <= two->val) {
+				return one;
+			} else {
+				return two;
+			}
+		} else if (one) {
+			return one;
+		} else if (two) {
+			return two;
+		} else {
+			return nullptr;
+		}
+		
+	};
+
+
+
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 		using NodePtr = ListNode*;
-		NodePtr curr1, curr2, temp1, temp2, newHead;
-		if (!list1 && list2) {
-			return list2;
-		} else if (list1 && !list2) {
-			return list1;e
-		} else if (!list1 && !list2) {
-			return newHead;
-		}
+		NodePtr curr1, curr2, prev, newHead;
+		if (min(list1, list2) == nullptr) {return nullptr;}
+		newHead, prev = min(list1, list2);
 		curr1 = list1;
 		curr2 = list2;
-		if (curr1->val <= curr2->val) {
-			newHead = curr1;
-		} else { 
-			newHead = curr2;
-		}
-		while (curr1->next != nullptr && curr2->next != nullptr) {
-			if (curr1->val <= curr2->val) {
-				temp1 = curr1->next;
-				temp2 = curr2->next;
-				curr1->next = curr2;
-				curr2->next = temp1;
-				curr1 = temp1;
-				curr2 = temp2;
+		while (curr1 != nullptr && curr2 != nullptr) {
+			if (min(curr1, curr2) == curr1) {
+				curr1 = curr1->next;
 			} else {
-				temp1 = curr2->next;
-				temp2 = curr1->next;
-				curr2->next = curr1;
-				curr1->next = temp1;
-				curr2 = temp1;
-				curr1 = temp2;
-			}
+				curr2 = curr2->next;
+			}	
+			prev->next = min(curr1, curr2);
+			prev = prev->next;
 		}
-
 		return newHead;
-	}
+	};
 };
