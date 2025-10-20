9a7ab0cf84293943591d3b3a5ddc28b2914e90aa
A7 <alenity.seven@gmail.com>
2025-10-20 21:45:35 +0100

    inital commit

--- LeetCode Problems/21.cpp
+++ LeetCode Problems/21.cpp
@@ -6,16 +6,45 @@ struct ListNode {
 	ListNode() : val(0), next(nullptr) {}
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode *next) : val(x), next(next) {}
-}
+};
 
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-		ListNode* curr1, curr2;
+		using NodePtr = ListNode*;
+		NodePtr curr1, curr2, temp1, temp2, newHead;
+		if (!list1 && list2) {
+			return list2;
+		} else if (list1 && !list2) {
+			return list1;e
+		} else if (!list1 && !list2) {
+			return newHead;
+		}
 		curr1 = list1;
 		curr2 = list2;
-		while (curr1.next != nullptr && curr2.next != nullptr) {
-			if (curr1.val )
+		if (curr1->val <= curr2->val) {
+			newHead = curr1;
+		} else { 
+			newHead = curr2;
 		}
+		while (curr1->next != nullptr && curr2->next != nullptr) {
+			if (curr1->val <= curr2->val) {
+				temp1 = curr1->next;
+				temp2 = curr2->next;
+				curr1->next = curr2;
+				curr2->next = temp1;
+				curr1 = temp1;
+				curr2 = temp2;
+			} else {
+				temp1 = curr2->next;
+				temp2 = curr1->next;
+				curr2->next = curr1;
+				curr1->next = temp1;
+				curr2 = temp1;
+				curr1 = temp2;
+			}
+		}
+
+		return newHead;
 	}
-}
+};
