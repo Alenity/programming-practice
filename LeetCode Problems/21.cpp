011b3fa3e4457fcca27737107e5bed17e1f91712
A7 <danieladeniyi17@outlook.com>
2025-10-20 22:50:43 +0100

    Problem 21 still doesn't work but I've gotten much closer

--- LeetCode Problems/21.cpp
+++ LeetCode Problems/21.cpp
@@ -11,7 +11,7 @@ struct ListNode {
 
 class Solution {
 public:
-	ListNode* min(ListNode* one, ListNode* two) {
+	ListNode* lesser(ListNode* one, ListNode* two) {
 		if (one != nullptr && two != nullptr) {
 			if (one->val <= two->val) {
 				return one;
@@ -25,27 +25,30 @@ public:
 		} else {
 			return nullptr;
 		}
-		
 	};
 
 
 
-	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+	ListNode* mergeTwoLists(ListNode* list1 = nullptr, ListNode* list2 = nullptr) {
 		using NodePtr = ListNode*;
 		NodePtr curr1, curr2, prev, newHead;
-		if (min(list1, list2) == nullptr) {return nullptr;}
-		newHead, prev = min(list1, list2);
+		newHead, prev = lesser(list1, list2);
 		curr1 = list1;
 		curr2 = list2;
 		while (curr1 != nullptr && curr2 != nullptr) {
-			if (min(curr1, curr2) == curr1) {
+			if (lesser(curr1, curr2) == curr1) {
 				curr1 = curr1->next;
 			} else {
 				curr2 = curr2->next;
-			}	
-			prev->next = min(curr1, curr2);
+			}
+			prev->next = lesser(curr1, curr2);
 			prev = prev->next;
+			printf("curr1 = %p, two = %p\n", (void*)curr1, (void*)curr2);
 		}
 		return newHead;
 	};
 };
+
+int main() {
+	return 0;
+}
