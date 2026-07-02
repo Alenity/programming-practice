73f5ae1b0db2fb7f2f1fabb76bd0357b1df6e1d1
Alenity <alenity.seven@gmail.com>
2026-07-02 15:10:34 +0100

    Added LearnCMake folder.

---
+++ projects/LearnCMake/Step0/build/CMakeFiles/hello.dir/HelloWorld.cxx.o.d
@@ -0,0 +1,23 @@
+CMakeFiles/hello.dir/HelloWorld.cxx.o: \
+ /home/a-seven/Code/programming-practice/projects/LearnCMake/cmake-tutorial-source/Step0/HelloWorld.cxx \
+ /usr/include/stdc-predef.h /usr/include/c++/15/cstdio \
+ /usr/include/c++/15/x86_64-suse-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/15/x86_64-suse-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/15/x86_64-suse-linux/bits/cpu_defines.h \
+ /usr/include/c++/15/pstl/pstl_config.h /usr/include/stdio.h \
+ /usr/include/bits/libc-header-start.h \
+ /usr/lib64/gcc/x86_64-suse-linux/15/include/stddef.h \
+ /usr/lib64/gcc/x86_64-suse-linux/15/include/stdarg.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h
